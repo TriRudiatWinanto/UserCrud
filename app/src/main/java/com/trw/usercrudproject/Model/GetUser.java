a7470c6f248337435282b1bf5234bdb507ad5646
TriRudiatWinanto <triwinantopmk@gmail.com>
2021-07-01 07:01:03 +0800

    UserCrud

---
+++ app/src/main/java/com/trw/usercrudproject/Model/GetUser.java
@@ -0,0 +1,28 @@
+package com.trw.usercrudproject.Model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class GetUser {
+    @SerializedName("data")
+    List<User> data;
+    @SerializedName("total")
+    String total;
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public List<User> getData() {
+        return data;
+    }
+
+    public void setData(List<User> data) {
+        this.data = data;
+    }
+}
